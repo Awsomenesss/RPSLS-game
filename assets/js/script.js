547f32053b6f1594205974275b3f52591b3b859f
Awsomenesss <>
2023-09-29 09:18:21 +0000

    set up basic structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function runGame(){};
+function updateAttemptCounter(){};
+function generateComputerChoice(){};
+function updateGameDisplay(){};
+function incrementComputerScore(){};
+function incrementPlayerScore(){};
+function calculateResult(){};
