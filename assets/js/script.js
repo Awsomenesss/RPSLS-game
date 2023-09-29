35acf38ffac07f15a1aa176304d7e0bf9f2bc1c8
Awsomenesss <>
2023-09-29 09:51:09 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {   
+             let choice = this.getAttribute("data-type");
+             alert(`You clicked ${choice}`);
+        });
+    }
+});
+
 function runGame(){};
 function updateAttemptCounter(){};
 function generateComputerChoice(){};
