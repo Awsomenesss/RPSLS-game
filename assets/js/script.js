b67f0de46ecab91c9f1162745cbcc6346e6b5c36
Awsomenesss <>
2023-09-29 17:43:01 +0000

    Added event listeners to handle button clicks for buttons with data-type attributes, allowing users to increase, decrease, and reset the number of attempts. Also added alerts to indicate button clicks for testing purposes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,242 +1,68 @@
-const computerChoiceDisplay = document.getElementById("computer_choice");
-const playerChoiceDisplay = document.getElementById("playersChoice");
-const resultDisplay = document.getElementById("result_display");
-const computerScore = document.getElementById("computer_score");
-const userScore = document.getElementById("user_score");
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.querySelectorAll("button[data-type]");
+    let maxClicksElement = document.getElementById("maxClicks");
 
-let computerLabel = document.getElementById("computer_label");
-let playerLabel = document.getElementById("user_label");
+    // Add click event listeners to all buttons
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            let gameType = this.getAttribute("data-type");
 
-var userChoice;
-var computerChoice;
-var result;
+            // Handle the increment and decrement buttons
+            if (gameType === "increase") {
+                increaseMaxClicks(maxClicksElement);
+            } else if (gameType === "decrease") {
+                decreaseMaxClicks(maxClicksElement);
+            } else if (gameType === "reset") {
+                resetMaxClicks(maxClicksElement);
+            } else {
 
-// Selects the rock weapon icon
-function selectRock() {
-    userChoice = "Rock";
-    playersChoice.innerHTML = '<button data-type="rock" class="btn btn--big btn--green">< i class="far fa-hand-rock" ></i ></button > ';
-    computerGenerator();
-    calculateWinner();
-}
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
 
-// Selects the paper weapon icon
-function selectPaper() {
-    userChoice = "Paper";
-    userChoiceDisplay.innerHTML = '<button id="Paper" class="btn_big btn_black"><i class="far fa-hand-paper weapon"></i></button>';
-    computerGenerator();
-    calculateWinner();
-}
-
-// Selects the scissors weapon icon
-function selectScissors() {
-    userChoice = "Scissors";
-    userChoiceDisplay.innerHTML = '<button id="Scissors" class="btn_big btn_red"><td></td><i class="far fa-hand-scissors weapon"></i></button>';
-    computerGenerator();
-    calculateWinner();
-}
-
-// Selects the lizard weapon icon
-function selectLizard() {
-    userChoice = "Lizard";
-    userChoiceDisplay.innerHTML = '<button id="Lizard" class="btn_big btn_yellow"><i class="far fa-hand-lizard weapon"></i></button>';
-    computerGenerator();
-    calculateWinner();
-}
-
-// Selects the spock weapon icon
-function selectSpock() {
-    userChoice = "Spock";
-    userChoiceDisplay.innerHTML = '<button id="Spock" class="btn_big btn_green"><i class="far fa-hand-spock weapon"></i></button>';
-    computerGenerator();
-    calculateWinner();
-}
-
-// Generates computer choice
-function computerGenerator() {
-    var computerChoiceIcon;
-
-    var randomNumber = Math.floor(Math.random() * 5) + 1;
-
-    if (randomNumber === 1) {
-        computerChoice = "Rock";
-        computerChoiceIcon = '<button id="Rock" class="btn_big btn_blue"><i class="far fa-hand-rock weapon"></i></button>';
-    }
-    if (randomNumber === 2) {
-        computerChoice = "Paper";
-        computerChoiceIcon = '<button id="Paper" class="btn_big btn_black"><i class="far fa-hand-paper weapon"></i></button>';
-    }
-    if (randomNumber === 3) {
-        computerChoice = "Scissors";
-        computerChoiceIcon = '<button id="Scissors" class="btn_big btn_red"><i class="far fa-hand-scissors weapon"></i></button>';
-    }
-    if (randomNumber === 4) {
-        computerChoice = "Lizard";
-        computerChoiceIcon = '<button id="Lizard" class="btn_big btn_yellow"><i class="far fa-hand-lizard weapon"></i></button>';
-    }
-    if (randomNumber === 5) {
-        computerChoice = "Spock";
-        computerChoiceIcon = '<button id="Spock" class="btn_big btn_green"><i class="far fa-hand-spock weapon"></i></button>';
-    }
-    computerChoiceDisplay.innerHTML = computerChoiceIcon;
-}
-
-// Compares user and computer choice and calculates a winner
-function calculateWinner() {
-    if (computerChoice === userChoice) {
-        result = "It's a draw!";
-        computerLabel.style.backgroundColor = "#f4c300";
-        playerLabel.style.backgroundColor = "#f4c300";
-        resultDisplay.innerHTML = result;
-    }
-    // Rock
-    if (userChoice === "Rock" && computerChoice === "Lizard") {
-        result = "Rock crushes Lizard, you win!";
-        resultDisplay.innerHTML = result;
-        incrementUserScore();
-    }
-    if (userChoice === "Rock" && computerChoice === "Scissors") {
-        result = "Rock crushes Scissors, you win!!";
-        resultDisplay.innerHTML = result;
-        incrementUserScore();
-    }
-    if (userChoice === "Rock" && computerChoice === "Spock") {
-        result = "Spock vaporizes Rock, you lose!";
-        resultDisplay.innerHTML = result;
-        incrementComputerScore();
-    }
-    if (userChoice === "Rock" && computerChoice === "Paper") {
-        result = "Paper covers Rock, you lose!";
-        resultDisplay.innerHTML = result;
-        incrementComputerScore();
-    }
-    // Lizard
-    if (userChoice === "Lizard" && computerChoice === "Paper") {
-        result = "Lizard eats Paper, you win!";
-        resultDisplay.innerHTML = result;
-        incrementUserScore();
-    }
-    if (userChoice === "Lizard" && computerChoice === "Spock") {
-        result = "Lizard poisons Spock, you win!";
-        resultDisplay.innerHTML = result;
-        incrementUserScore();
-    }
-    if (userChoice === "Lizard" && computerChoice === "Rock") {
-        result = "Rock crushes Lizard, you lose!";
-        resultDisplay.innerHTML = result;
-        incrementComputerScore();
-    }
-    if (userChoice === "Lizard" && computerChoice === "Scissors") {
-        result = "Scissors decapitates Lizard, you lose!";
-        resultDisplay.innerHTML = result;
-        incrementComputerScore();
-    }
-    // Spock
-    if (userChoice === "Spock" && computerChoice === "Scissors") {
-        result = "Spock smashes Scissors, you win!";
-        resultDisplay.innerHTML = result;
-        incrementUserScore();
-    }
-    if (userChoice === "Spock" && computerChoice === "Rock") {
-        result = "Spock vaporizes Rock, you win!";
-        resultDisplay.innerHTML = result;
-        incrementUserScore();
-    }
-    if (userChoice === "Spock" && computerChoice === "Paper") {
-        result = "Paper disproves Spock, you lose!";
-        resultDisplay.innerHTML = result;
-        incrementComputerScore();
-    }
-    if (userChoice === "Spock" && computerChoice === "Lizard") {
-        result = "Lizard poisons Spock, you lose!";
-        resultDisplay.innerHTML = result;
-        incrementComputerScore();
-    }
-    // Scissors
-    if (userChoice === "Scissors" && computerChoice === "Paper") {
-        result = "Scissors cuts Paper, you win!";
-        resultDisplay.innerHTML = result;
-        incrementUserScore();
-    }
-    if (userChoice === "Scissors" && computerChoice === "Lizard") {
-        result = "Scissors decapitates Lizard, you win!";
-        resultDisplay.innerHTML = result;
-        incrementUserScore();
-    }
-    if (userChoice === "Scissors" && computerChoice === "Rock") {
-        result = "Rock crushes Scissors, you lose!";
-        resultDisplay.innerHTML = result;
-        incrementComputerScore();
-    }
-    if (userChoice === "Scissors" && computerChoice === "Spock") {
-        result = "Spock smashes Scissors, you lose!";
-        resultDisplay.innerHTML = result;
-        incrementComputerScore();
-    }
-    // Paper
-    if (userChoice === "Paper" && computerChoice === "Rock") {
-        result = "Paper covers Rock, you win!";
-        resultDisplay.innerHTML = result;
-        incrementUserScore();
-    }
-    if (userChoice === "Paper" && computerChoice === "Spock") {
-        result = "Paper disproves Spock, you win!";
-        resultDisplay.innerHTML = result;
-        incrementUserScore();
-    }
-    if (userChoice === "Paper" && computerChoice === "Scissors") {
-        result = "Scissors cuts Paper, you lose!";
-        resultDisplay.innerHTML = result;
-        incrementComputerScore();
-    }
-    if (userChoice === "Paper" && computerChoice === "Lizard") {
-        result = " Lizard eats Paper, you lose!";
-        resultDisplay.innerHTML = result;
-        incrementComputerScore();
+function increaseMaxClicks(maxClicksElement) {
+    let maxClicks = parseInt(maxClicksElement.innerText);
+    if (maxClicks < 10) {
+        maxClicks++;
+        maxClicksElement.innerText = maxClicks;
     }
 }
 
-// Gets current computer score and increments by 1
-function incrementComputerScore() {
-    // Gets the current computer score from the DOM and increments it by 1
-    let oldScore = computerScore.innerText;
-    computerScore.innerText = ++oldScore;
-    let newScore = computerScore.innerHTML;
-    // Styles the player and computer badges to red or green dependant on the winner
-    computerLabel.style.backgroundColor = "#009f3d";
-    playerLabel.style.backgroundColor = "#df0024";
-
-    if (newScore == 3) {
-        resultDisplay.innerHTML = "<img class='gif' src='assets/images/you_lose.gif'/> <a class='index_btn' href='start_game.html'>New Game</a>";
-        const choiceArea = document.getElementsByClassName("choice_area");
-        const weaponsArea = document.getElementsByClassName("weapons_area");
-        while (choiceArea.length > 0) {
-            choiceArea[0].parentNode.removeChild(choiceArea[0]);
-        }
-        while (weaponsArea.length > 0) {
-            weaponsArea[0].parentNode.removeChild(weaponsArea[0]);
-        }
+function decreaseMaxClicks(maxClicksElement) {
+    let maxClicks = parseInt(maxClicksElement.innerText);
+    if (maxClicks > 1) {
+        maxClicks--;
+        maxClicksElement.innerText = maxClicks;
     }
 }
 
-// Gets current user score and increments 1
-function incrementUserScore() {
-    // Gets the current player score from the DOM and increments it by 1
-    let oldScore = userScore.innerText;
-    userScore.innerText = ++oldScore;
-    let newScore = userScore.innerHTML;
-    // Styles the player and computer badges to red or green dependant on the winner
-    computerLabel.style.backgroundColor = "#df0024";
-    playerLabel.style.backgroundColor = "#009f3d";
+function resetMaxClicks(maxClicksElement) {
+    // Reset maxClicks to 1
+    maxClicksElement.innerText = "1";
+}
 
-    if (newScore == 3) {
-        resultDisplay.innerHTML = "<img class='gif' src='assets/images/winner.gif'/> <a class='index_btn' href='start_game.html'>New Game</a>";
-        const choiceArea = document.getElementsByClassName("choice_area");
-        const weaponsArea = document.getElementsByClassName("weapons_area");
-        while (choiceArea.length > 0) {
-            choiceArea[0].parentNode.removeChild(choiceArea[0]);
-        }
-        while (weaponsArea.length > 0) {
-            weaponsArea[0].parentNode.removeChild(weaponsArea[0]);
-        }
-    }
+
+function runGame() {
+
+}
+function decideCompChoice() {
+
+}
+function results() {
+
+}
+function showPlayerScore() {
+
+}
+function showCompScore() {
+
+}
+function playerScore() {
+}
+function determineResult() {
+
 }
