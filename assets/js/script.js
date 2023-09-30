f58c75fc4b974e38964d0674e47cbf2fcf9d85ac
Awsomenesss <>
2023-09-30 22:51:06 +0000

    separated generateComputerChoice and updatePlayerChoice to avoide bugs that came with the rungame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,13 @@ const computerChoices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.querySelectorAll("button[data-type]");
-    let maxClicksElement = document.getElementById("maxClicks");
-    const playerChoiceElement = document.getElementById("playersChoice");
-    const compChoiceElement = document.getElementById("compChoice");
+    const buttons = document.querySelectorAll("button[data-type]");
+    const maxClicksElement = document.getElementById("maxClicks");
 
-    // Add click event listeners to all buttons
-    for (let button of buttons) {
+    for (const button of buttons) {
         button.addEventListener("click", function () {
-            let gameType = this.getAttribute("data-type");
+            const gameType = this.getAttribute("data-type");
 
-            // Handle the increment and decrement buttons
             if (gameType === "increase") {
                 increaseMaxClicks(maxClicksElement);
             } else if (gameType === "decrease") {
@@ -20,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
             } else if (gameType === "reset") {
                 resetMaxClicks(maxClicksElement);
             } else {
-                runGame(gameType, playerChoiceElement, compChoiceElement);
+                runGame(gameType);
             }
         });
     }
@@ -47,32 +43,25 @@ function resetMaxClicks(maxClicksElement) {
 }
 
 function runGame(gameType) {
-    const computerChoiceIndex = Math.floor(Math.random() * computerChoices.length);
-    const computerChoice = computerChoices[computerChoiceIndex];
+    const computerChoice = generateComputerChoice();
+    updatePlayerChoice(gameType);
+    updateComputerChoice(computerChoice);
+    calculateWinner(gameType, computerChoice);
+}
 
-    // Update the player's choice
+function generateComputerChoice() {
+    const computerChoiceIndex = Math.floor(Math.random() * computerChoices.length);
+    return computerChoices[computerChoiceIndex];
+}
+
+function updatePlayerChoice(gameType) {
     const playerChoiceButton = document.querySelector(`button[data-type="${gameType}"]`);
     const playerChoiceHTML = playerChoiceButton.outerHTML;
-
     document.getElementById("playersChoice").innerHTML = playerChoiceHTML;
+}
 
-    // Update the computer's choice
+function updateComputerChoice(computerChoice) {
     const compChoiceButton = document.querySelector(`button[data-type="${computerChoice.toLowerCase()}"]`);
     const compChoiceHTML = compChoiceButton.outerHTML;
-
     document.getElementById("compChoice").innerHTML = compChoiceHTML;
-}
-
-function results() {
-
-}
-function showPlayerScore() {
-
-}
-function showCompScore() {
-
-}
-
-function determineResult() {
-
 }
